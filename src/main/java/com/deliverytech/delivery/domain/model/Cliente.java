f35c7e5e39df5fb342ddecc3f261c2ef5f2a176d
VictorHFMartins <navictor70@gmail.com>
2025-10-29 11:38:30 -0300

    Estrutura de cliente e configurações do bd

---
+++ src/main/java/com/deliverytech/delivery/domain/model/Cliente.java
@@ -0,0 +1,40 @@
+package com.deliverytech.delivery.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "clientes")
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false, length = 120)
+    private String nome;
+
+    private String email;
+
+    private String telefone;
+
+    private String endereco;
+
+    @Column(name = "data_cadastro")
+    private LocalDateTime dataCadastro;
+
+    @Column(nullable = false)
+    private Boolean ativo;
+
+    public void inativar() {
+        this.ativo = false;
+    }
+
+
+}
